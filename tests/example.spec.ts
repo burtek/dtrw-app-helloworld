aed0b26e774af16ad17cbe67fac074d25afe7d0e
Bartosz <burtekdotryw@gmail.com>
2026-07-28 10:32:12 +0200

    test: fix

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,5 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('');
 
-  await expect(page.locator('body')).toHaveText(/Hello World/);
+  await expect(page.locator('body')).toHaveText(/Response: Hello world/);
 });
