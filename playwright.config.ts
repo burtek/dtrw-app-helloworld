d1f252d1c909d9d682051f49c185b07eb5744cf3
Bartosz <2940958+burtek@users.noreply.github.com>
2026-07-28 11:53:26 +0200

    feat: use playwright container for running tests (#48)
    * feat: use playwright container for tunning tests
    
    * chore: reduce how many times tests are run in single workflow
    
    * chore: use github reporter on CI
    
    * chore: no need to install deps in container mode
    
    * chore: update playwright config
    
    * chore: rework workflow in terms of alpine build

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,7 +9,8 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
 
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: process.env.CI ? [['github'], ['html', { open: 'never' }], ['list']] : 'html',
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+test('renders response from api', async ({ page }) => {
   await page.goto('');
 
   await expect(page.locator('body')).toHaveText(/Response: Hello world/);
