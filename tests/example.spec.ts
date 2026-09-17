822e6e0be90e653d2b8d3cbebd5fbc4b328e8e3f
Bartosz <burtekdotryw@gmail.com>
2026-09-17 14:38:47 +0200

    fix: test strings

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,6 +3,6 @@ import { test, expect } from '@playwright/test';
 test('renders response from api', async ({ page }) => {
   await page.goto('');
 
-  await expect(page.locator('body')).toHaveText(/Response1: Hello world from backend/);
-  await expect(page.locator('body')).toHaveText(/Response2: Hello world from backend-sqlite/);
+  await expect(page.locator('body')).toHaveText(/Response1: hello world from backend/);
+  await expect(page.locator('body')).toHaveText(/Response2: hello world from backend-sqlite/);
 });
