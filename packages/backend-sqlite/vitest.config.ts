ed5671234ae90a7a666c9866f549913e7f81dc42
Bartosz <2940958+burtek@users.noreply.github.com>
2026-09-17 16:08:09 +0200

    fix: playwright tests on CI (#119)
    * fix: playwright tests on CI
    
    * fix: tests
    
    * fix: loading .env.test
    
    * fix: linter issues
    
    * fix: test strings
    
    * fix: Dockerfile dependencies
    
    * fix: pnpm install with filter

--- packages/backend-sqlite/vitest.config.ts
+++ packages/backend-sqlite/vitest.config.ts
@@ -1,8 +1,10 @@
+import { resolve } from 'node:path';
+
 import { config } from 'dotenv';
 import { defineConfig } from 'vitest/config';
 
 
-config({ path: '.env.test' });
+config({ path: resolve(import.meta.dirname, '.env.test') });
 
 // https://vite.dev/config/
 export default defineConfig({
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,5 +3,6 @@ import { test, expect } from '@playwright/test';
 test('renders response from api', async ({ page }) => {
   await page.goto('');
 
-  await expect(page.locator('body')).toHaveText(/Response: Hello world/);
+  await expect(page.locator('body')).toHaveText(/Response1: hello world from backend/);
+  await expect(page.locator('body')).toHaveText(/Response2: hello world from backend-sqlite/);
 });
